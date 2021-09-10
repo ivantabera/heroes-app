e55f727442607bb6ae9d41c5c81e71587ca2e6ce
ivantabera <ivantabera19@gmail.com>
2021-09-10 10:19:12 -0500

    Se realizaron los componentes necesarios para la base de la aplicacion de heroes

---
+++ src/routers/AppRouter.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route
+} from 'react-router-dom';
+
+import { LoginScreen } from '../components/login/LoginScreen';
+import { MarvelScreen } from '../components/marvel/MarvelScreen';
+
+import { Navbar } from '../components/ui/Navbar';
+
+export const AppRouter = () => {
+    return (
+        <Router>
+            <>
+                <Navbar />
+                
+                <Switch>
+                    <Route exact path="/login" component={LoginScreen} />
+                    <Route exact path="/" component={ MarvelScreen } />
+                </Switch>
+            </>
+        </Router>
+    );
+}
