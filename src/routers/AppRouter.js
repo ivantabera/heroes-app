8c3fbcfc604aa94f8bd3c38f9f17862fb27ce98a
ivantabera <ivantabera19@gmail.com>
2021-09-23 12:44:11 -0500

    Se realizo el PublicRoute para las rutas publicas mientras no se este loggeado, y se implemento en el AppRouter

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,14 +1,16 @@
 import React, { useContext } from 'react';
+
 import {
     BrowserRouter as Router,
-    Switch,
-    Route
+    Switch
 } from 'react-router-dom';
+
 import { AuthContext } from '../auth/AuthContext';
+import { PrivateRoute } from './PrivateRoute';
+import { PublicRoute } from './PublicRoute';
 
 import { LoginScreen } from '../components/login/LoginScreen';
 import { DashboardRoutes } from './DashboardRoutes';
-import { PrivateRoute } from './PrivateRoute';
 
 export const AppRouter = () => {
 
@@ -18,7 +20,13 @@ export const AppRouter = () => {
         <Router>
             <>  
                 <Switch>
-                    <Route exact path="/login" component={LoginScreen} />
+                    <PublicRoute 
+                        exact 
+                        path="/login" 
+                        component={LoginScreen} 
+                        isAuthenticated={ user.logged }
+                    />
+
                     <PrivateRoute 
                         path="/" 
                         component={ DashboardRoutes } 
