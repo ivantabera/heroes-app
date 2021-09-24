a04bb42e709142acf825451d5e3c80a2618ca479
ivantabera <ivantabera19@gmail.com>
2021-09-24 13:50:37 -0500

    pruebas en PrivateRoute debe de mostrar el componente si esta autenticado y guardar localStorage

---
+++ src/tests/routers/PrivateRoute.test.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { mount } from 'enzyme';
+import { PrivateRoute } from '../../routers/PrivateRoute';
+import { MemoryRouter } from 'react-router-dom';
+
+describe('Pruebas en <PrivateRoute />', () => {
+
+    const props = {
+        location: {
+            pathname: '/marvel'
+        }
+    }
+    
+    test('debe de mostrar el componente si esta autenticado y guardar localStorage', () => {
+        
+        const wrapper = mount(
+
+            <MemoryRouter>
+                <PrivateRoute 
+                    isAuthenticated={true}
+                    component={ () => <span>Listo!</span> }
+                    { ...props }
+                />
+            </MemoryRouter>
+            
+        );
+
+        expect( wrapper.find('span').exist() ).toBe(true);
+
+    })
+    
+})
